6d54f63d8ee1a17e7e6745fb4c02b90a9a283f51
m-saleh96 <scyllies@gmail.com>
2023-05-24 12:11:55 +0300

    get id of book from activate route and use service for fun getbookdetails this show details on page

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BooksService } from '../services/books.service';
 
 @Component({
   selector: 'app-book-details',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-details.component.css']
 })
 export class BookDetailsComponent {
+  id!:number;
+  book!:any;
 
+  constructor(private route:ActivatedRoute , private bookService:BooksService){}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+    this.id = (params['id'])
+    console.log(this.id);
+    });
+
+    this.bookService.getBookDetails(this.id).subscribe((res:any)=>this.book=res)
+    
+    
+  }
+  
+
+  
 }
